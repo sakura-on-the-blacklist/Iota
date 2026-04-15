b5eedd9d94d5b9ec47da7135ce2cadb92b24b709
Sakura Kano <kanosakura@icloud.com>
2026-04-15 21:10:11 +0800

    Added logout

--- app/src/main/java/edu/ph/iota/activities/HomeActivity.kt
+++ app/src/main/java/edu/ph/iota/activities/HomeActivity.kt
@@ -1,21 +1,52 @@
 package edu.ph.iota.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import edu.ph.iota.MainActivity
 import edu.ph.iota.R
+import edu.ph.iota.databinding.ActivityHomeBinding
+import edu.ph.iota.viewmodels.HomeViewModel
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setViews()
+    }
+
+    private fun setViews() {
+
+
+        binding.logoutbtn.setOnClickListener {
+            onLogout()
+        }
+
+
+    }
+
+    private fun onLogout(){
+        viewModel.preferenceManager.onLogout()
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+
     }
 }
