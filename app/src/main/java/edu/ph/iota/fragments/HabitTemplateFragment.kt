7e28b4f8048178bcd0bac303fadff86b6ae0d7b7
Theresa Isabelle P. Arcelo <20100707@usc.edu.ph>
2026-04-21 00:44:22 +0800

    Added onboarding, habit creation and habit template screens

---
+++ app/src/main/java/edu/ph/iota/fragments/HabitTemplateFragment.kt
@@ -0,0 +1,38 @@
+package edu.ph.iota.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import edu.ph.iota.R
+import edu.ph.iota.databinding.FragmentHabitTemplateBinding
+
+class HabitTemplateFragment : Fragment() {
+
+    private var _binding: FragmentHabitTemplateBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHabitTemplateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnContinue.setOnClickListener {
+            it.findNavController().navigate(R.id.action_habitTemplate_to_habitSettings)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
